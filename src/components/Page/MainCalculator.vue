b111bc7f698b281d7cfa4dd4dd4f13a87d3f89a1
furxx2000 <furxx2000@gmail.com>
2022-07-31 13:35:04 +0800

    Adding new logic for better experience

--- src/components/Page/MainCalculator.vue
+++ src/components/Page/MainCalculator.vue
@@ -13,32 +13,53 @@ import { reactive } from "vue";
 interface input {
   curVal: string;
   lastOperator: string;
+  isTyping: boolean;
   storedVal: number[];
 }
 
 const inputVal = reactive<input>({
   curVal: "",
   lastOperator: "",
+  isTyping: false,
   storedVal: [],
 });
 
+function initialize(isInitializeCurVal = false) {
+  if (isInitializeCurVal) {
+    inputVal.curVal = "";
+  }
+  inputVal.lastOperator = "";
+  inputVal.storedVal = [];
+}
+
 function getKey(val: string) {
   if (val === "+" || val === "-" || val === "x" || val === "/") {
+    if (inputVal.isTyping) return;
+
+    inputVal.isTyping = true;
+
+    if (inputVal.lastOperator) {
+      calculateTotalValue(+inputVal.curVal, inputVal.lastOperator);
+      inputVal.lastOperator = val;
+      return;
+    }
+
     inputVal.storedVal.push(+inputVal.curVal);
-    inputVal.curVal = "";
     inputVal.lastOperator = val;
+
     return;
   }
+
   if (val === "del") {
     inputVal.curVal = "";
     return;
   }
+
   if (val === "reset") {
-    inputVal.curVal = "";
-    inputVal.lastOperator = "";
-    inputVal.storedVal = [];
+    initialize(true);
     return;
   }
+
   if (val === "=") {
     if (
       !inputVal.curVal ||
@@ -46,24 +67,27 @@ function getKey(val: string) {
       (inputVal.curVal === "0" && inputVal.storedVal[0] === 0)
     )
       return;
-    calculate(+inputVal.curVal, inputVal.lastOperator);
-    inputVal.curVal = inputVal.storedVal[0].toString().slice(0, 9);
-    inputVal.lastOperator = "";
-    inputVal.storedVal = [];
+
+    calculateTotalValue(+inputVal.curVal, inputVal.lastOperator);
+    initialize();
     return;
   }
 
   if (
-    inputVal.curVal.length >= 9 ||
+    inputVal.curVal.length >= 10 ||
     (inputVal.curVal === "0" && val !== ".") ||
     (inputVal.curVal.includes(".") && val === ".")
   )
     return;
 
+  if (inputVal.isTyping) {
+    inputVal.curVal = "";
+    inputVal.isTyping = false;
+  }
   inputVal.curVal += val;
 }
 
-function calculate(val: number, operator: string): void {
+function calculateTotalValue(val: number, operator: string): void {
   if (operator === "+") {
     inputVal.storedVal[0] += val;
   } else if (operator === "-") {
@@ -73,6 +97,7 @@ function calculate(val: number, operator: string): void {
   } else if (operator === "/") {
     inputVal.storedVal[0] /= val;
   }
+  inputVal.curVal = inputVal.storedVal[0].toString().slice(0, 10);
 }
 </script>
 
