1cb556be130b3fa9c8060cbfc1ce090ac10d912e
furxx2000 <furxx2000@gmail.com>
2022-08-04 13:44:37 +0800

    Creat new UI component

---
+++ src/components/UI/BaseKeyButton.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { toRefs } from "@vue/reactivity";
+import {} from "./TheHeader.vue";
+
+interface Props {
+  name: string;
+  text: string;
+  isSelected: boolean;
+}
+
+const props = defineProps<Props>();
+const { name, text, isSelected } = toRefs(props);
+</script>
+
+<template>
+  <button :class="name" :aria-selected="isSelected" role="tab" tabindex="-1">
+    <span>{{ text }}</span>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+button {
+  position: relative;
+  appearance: none;
+  border: none;
+  border-radius: 6px;
+  font-size: $fs;
+  transition: all 0.05s ease-in;
+  cursor: pointer;
+  @include grid(0);
+  @include desktop {
+    border-radius: 10px;
+    font-size: 36px;
+  }
+}
+
+button:active {
+  transform: translateY(4px);
+  box-shadow: none;
+}
+
+button[aria-selected="true"] {
+  transform: translateY(4px);
+  box-shadow: none;
+  background-color: var(--bg-key-1-shadow);
+}
+
+button > span {
+  position: absolute;
+  place-self: center;
+  padding-top: 6px;
+}
+</style>
