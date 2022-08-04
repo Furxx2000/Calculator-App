84acb016cf6fe14ea8ea1ffa1cc12a87913aedc2
furxx2000 <furxx2000@gmail.com>
2022-08-04 13:46:46 +0800

    Add class name display-window for the div

--- src/components/Layout/TheDisplayWindow.vue
+++ src/components/Layout/TheDisplayWindow.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { toRefs } from "@vue/reactivity";
-import { computed, ref } from "vue";
+import { computed } from "vue";
 import {} from "./TheHeader.vue";
 
 interface Props {
@@ -17,24 +17,27 @@ const val = computed(() => {
 </script>
 
 <template>
-  <div>{{ val }}</div>
+  <div class="display-window">{{ val }}</div>
 </template>
 
 <style lang="scss" scoped>
-div {
+.display-window {
   display: grid;
   align-items: center;
   appearance: none;
   outline: none;
   width: 100%;
+  color: var(--text-header);
   padding-inline: 1.6rem;
   padding-top: 0.5rem;
   font: inherit;
   font-size: 40px;
   border: 0;
-  color: var(--text-header);
-  background-color: var(--screen-bg);
   border-radius: 8px;
+  background-color: var(--screen-bg);
   text-align: right;
+  @include desktop {
+    font-size: 48px;
+  }
 }
 </style>
