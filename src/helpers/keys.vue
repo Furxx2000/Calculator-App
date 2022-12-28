72098a06a990ac21fe649baa123b7535a2e1618a
Danny Wen <danny_wen@trendmicro.com>
2022-12-28 19:22:29 +0800

    Rename

---
+++ src/helpers/keys.vue
@@ -0,0 +1,9 @@
+export const keysData = [ { name: 'zero', text: '0', }, { name: 'one', text:
+'1', }, { name: 'two', text: '2', }, { name: 'three', text: '3', }, { name:
+'four', text: '4', }, { name: 'five', text: '5', }, { name: 'six', text: '6', },
+{ name: 'seven', text: '7', }, { name: 'eight', text: '8', }, { name: 'nine',
+text: '9', }, { name: 'decimal', text: '.', }, { name: 'add', text: '+',
+isSelected: false, }, { name: 'subtract', text: '-', isSelected: false, }, {
+name: 'multiply', text: 'x', isSelected: false, }, { name: 'divide', text: '/',
+isSelected: false, }, { name: 'clear', text: 'del', }, { name: 'reset', text:
+'reset', }, { name: 'equals', text: '=', }, ];
