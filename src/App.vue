cc10670f7e7e6fd809b1b649fb66aef7717d1ebc
Danny Wen <danny_wen@trendmicro.com>
2022-12-28 10:57:24 +0800

    Change double quotes to single quote

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,15 @@
 </template>
 
 <script setup lang="ts">
-import { onBeforeMount } from "@vue/runtime-core";
+import { onBeforeMount } from '@vue/runtime-core';
 
 onBeforeMount(() => {
-  document.body.classList.add("theme-1");
+  document.body.classList.add('theme-1');
 });
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=League+Spartan:wght@700&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@700&display=swap');
 
 *,
 *::before,
@@ -22,7 +22,7 @@ onBeforeMount(() => {
 body {
   line-height: 1.5;
   height: 100vh;
-  font-family: "League Spartan", sans-serif;
+  font-family: 'League Spartan', sans-serif;
   background-color: var(--main-bg);
   margin: 0;
 }
